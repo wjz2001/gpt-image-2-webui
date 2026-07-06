703bdf36159f57c1eed2fb11857dd5b10b5c020b
wjz2001 <wjz2001@users.noreply.github.com>
2026-07-06 23:27:31 +0800

    解决Next.js 16 开发模式默认阻止跨来源访问 dev 资源的问题

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const assetPrefix = process.env.NEXT_ASSET_PREFIX;
 const nextConfig: NextConfig = {
   output: "standalone",
   assetPrefix: assetPrefix || undefined,
+  allowedDevOrigins: ["127.0.0.1", "192.168.220.1"],
 };
 
 export default nextConfig;
